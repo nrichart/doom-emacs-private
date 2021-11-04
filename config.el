31eff3fa0ed23e0d75896454e89f05c64586c261
Nicolas Richart <nicolas.richart@epfl.ch>
2021-11-04 10:30:53 +0100

    Org ROAM

--- config.el
+++ config.el
@@ -9,111 +9,113 @@
 (load! "lisp/poly-yaml-jinja2")
 
 (setq ;;tab-width 2
-      tab-always-indent t
-      indent-tabs-mode nil
-      fill-column 80
+ tab-always-indent t
+ indent-tabs-mode nil
+ fill-column 80
 
-      user-full-name "Nicolas Richart"
-      user-mail-address "nicolas.richart@gmail.ch"
-      
-      epa-file-encrypt-to user-mail-address
+ user-full-name "Nicolas Richart"
+ user-mail-address "nicolas.richart@gmail.ch"
 
-      projectile-project-seatrch-path "~/dev"
+ epa-file-encrypt-to user-mail-address
 
-      ;; ensure fill-paragraph takes doxygen @ markers as start of new
-      ;; paragraphs properly
-      paragraph-start "^[ ]*\\(//+\\|\\**\\)[ ]*\\([ ]*$\\|@param\\)\\|^\f"
+ projectile-project-seatrch-path "~/dev"
 
-      ;; Line numbers are pretty slow all around. The performance boost of
-      ;; disabling them outweighs the utility of always keeping them on.
-      ;; display-line-numbers-type nil
+ ;; ensure fill-paragraph takes doxygen @ markers as start of new
+ ;; paragraphs properly
+ paragraph-start "^[ ]*\\(//+\\|\\**\\)[ ]*\\([ ]*$\\|@param\\)\\|^\f"
 
-      ;; On-demand code completion. I don't often need it.
-      company-idle-delay nil
+ ;; Line numbers are pretty slow all around. The performance boost of
+ ;; disabling them outweighs the utility of always keeping them on.
+ ;; display-line-numbers-type nil
 
-      ;; lsp-ui-sideline is redundant with eldoc and much more invasive, so
-      ;; ;; disable it by default.
-      lsp-ui-sideline-enable nil
-      ;; lsp-enable-indentation nil
-      lsp-enable-on-type-formatting t
-      ;; lsp-enable-symbol-highlighting nil
-      ;; lsp-enable-file-watchers nil
+ ;; On-demand code completion. I don't often need it.
+ company-idle-delay nil
 
-      lsp-ui-peek-always-show t
-      lsp-ui-flycheck-live-reporting t
-      lsp-ui-flycheck-enable t
+ ;; lsp-ui-sideline is redundant with eldoc and much more invasive, so
+ ;; ;; disable it by default.
+ lsp-ui-sideline-enable nil
+ ;; lsp-enable-indentation nil
+ lsp-enable-on-type-formatting t
+ ;; lsp-enable-symbol-highlighting nil
+ ;; lsp-enable-file-watchers nil
 
-      lsp-file-watch-ignored (quote
-                              ("[/\\\\]\\.git$"
-                               "[/\\\\]\\.hg$"
-                               "[/\\\\]\\.bzr$"
-                               "[/\\\\]_darcs$"
-                               "[/\\\\]\\.svn$"
-                               "[/\\\\]_FOSSIL_$"
-                               "[/\\\\]\\.idea$"
-                               "[/\\\\]\\.ensime_cache$"
-                               "[/\\\\]\\.eunit$"
-                               "[/\\\\]node_modules$"
-                               "[/\\\\]\\.fslckout$"
-                               "[/\\\\]\\.tox$"
-                               "[/\\\\]\\.stack-work$"
-                               "[/\\\\]\\.bloop$"
-                               "[/\\\\]\\.metals$"
-                               "[/\\\\]target$"
-                               "[/\\\\]\\.deps$"
-                               "[/\\\\]autom4te.cache$"
-                               "[/\\\\]\\.reference$"
-                               "[/\\\\]build.*$"
-                               "[/\\\\]test$"
-                               "[/\\\\]extra-packages$"
-                               "[/\\\\]examples$"))
+ lsp-ui-peek-always-show t
+ lsp-ui-flycheck-live-reporting t
+ lsp-ui-flycheck-enable t
 
-      ;; Disable help mouse-overs for mode-line segments (i.e. :help-echo text).
-      ;; They're generally unhelpful and only add confusing visual clutter.
-      mode-line-default-help-echo nil
-      show-help-function nil
+ lsp-file-watch-ignored (quote
+                         ("[/\\\\]\\.git$"
+                          "[/\\\\]\\.hg$"
+                          "[/\\\\]\\.bzr$"
+                          "[/\\\\]_darcs$"
+                          "[/\\\\]\\.svn$"
+                          "[/\\\\]_FOSSIL_$"
+                          "[/\\\\]\\.idea$"
+                          "[/\\\\]\\.ensime_cache$"
+                          "[/\\\\]\\.eunit$"
+                          "[/\\\\]node_modules$"
+                          "[/\\\\]\\.fslckout$"
+                          "[/\\\\]\\.tox$"
+                          "[/\\\\]\\.stack-work$"
+                          "[/\\\\]\\.bloop$"
+                          "[/\\\\]\\.metals$"
+                          "[/\\\\]target$"
+                          "[/\\\\]\\.deps$"
+                          "[/\\\\]autom4te.cache$"
+                          "[/\\\\]\\.reference$"
+                          "[/\\\\]build.*$"
+                          "[/\\\\]test$"
+                          "[/\\\\]extra-packages$"
+                          "[/\\\\]examples$"))
 
-      global-prettify-symbols-mode nil
-      prettify-symbols-mode -1
+ ;; Disable help mouse-overs for mode-line segments (i.e. :help-echo text).
+ ;; They're generally unhelpful and only add confusing visual clutter.
+ mode-line-default-help-echo nil
+ show-help-function nil
 
-      c-default-style (quote
-                       ((c++-mode . "akantu")
-                        (java-mode . "java")
-                        (awk-mode . "awk")
-                        (other . "doom")))
+ global-prettify-symbols-mode nil
+ prettify-symbols-mode -1
 
-      clang-format-executable "clang-format-14"
-      lsp-clients-clangd-executable "clangd-14"
+ c-default-style (quote
+                  ((c++-mode . "akantu")
+                   (java-mode . "java")
+                   (awk-mode . "awk")
+                   (other . "doom")))
 
-      todotxt-file "/keybase/private/networms/todo/todo.txt"
+ clang-format-executable "clang-format-14"
+ lsp-clients-clangd-executable "clangd-14"
 
-      magit-git-executable "git"
+ todotxt-file "/keybase/private/networms/todo/todo.txt"
+ org-roam-db-location "/keybase/private/networms/roam/org-roam.db"
+ org-roam-directory "/keybase/private/networms/roam/"
 
-      hl-todo-keyword-faces (quote
-                             (("TODO" warning bold)
-                              ("FIXME" error bold)
-                              ("HACK" font-lock-constant-face bold)
-                              ("REVIEW" font-lock-keyword-face bold)
-                              ("NOTE" success bold)
-                              ("DEPRECATED" font-lock-doc-face bold)
-                              ("\\todo" warning bold)
-                              ("\\warning" warning bold)
-                              ("\\deprecated" font-lock-doc-face bold)))
+ magit-git-executable "git"
 
-      safe-local-variable-values (quote
-                                  ((c-file-style . akantu)
-                                   (flycheck-checker . c/c++-clang-tidy)
-                                   (eval set-background-color "#000015")
-                                   (projectile-enable-caching . t)
-                                   (projectile-project-name . "Akantu[master]")))
+ hl-todo-keyword-faces (quote
+                        (("TODO" warning bold)
+                         ("FIXME" error bold)
+                         ("HACK" font-lock-constant-face bold)
+                         ("REVIEW" font-lock-keyword-face bold)
+                         ("NOTE" success bold)
+                         ("DEPRECATED" font-lock-doc-face bold)
+                         ("\\todo" warning bold)
+                         ("\\warning" warning bold)
+                         ("\\deprecated" font-lock-doc-face bold)))
 
-      magit-revision-show-gravatars '("^Author:     " . "^Commit:     ")
-      magit-git-executable "git"
+ safe-local-variable-values (quote
+                             ((c-file-style . akantu)
+                              (flycheck-checker . c/c++-clang-tidy)
+                              (eval set-background-color "#000015")
+                              (projectile-enable-caching . t)
+                              (projectile-project-name . "Akantu[master]")))
 
-      compilation-scroll-output 'first-error
+ magit-revision-show-gravatars '("^Author:     " . "^Commit:     ")
+ magit-git-executable "git"
 
-      vterm-module-cmake-args "-DUSE_SYSTEM_LIBVTERM=yes"
-      )
+ compilation-scroll-output 'first-error
+
+ vterm-module-cmake-args "-DUSE_SYSTEM_LIBVTERM=yes"
+ )
 
 (setq tramp-remote-path (quote
                          (tramp-own-remote-path
@@ -130,59 +132,61 @@
 (add-to-list 'auto-mode-alist '("\\.F90\\'" . f90-mode))
 
 (map! "<f9>"     #'projectile-compile-project
-      "<f5>"     #'clang-format-buffer
       "C-c ;"    #'comment-region
       "M-g"      #'goto-line
 
+      (:when (featurep! :editor format)
+       "<f5>"     #'+format/buffer
+
       ;;; treemacs
-      (:when (featurep! :ui treemacs)
+       (:when (featurep! :ui treemacs)
         "<f8>"   #'+treemacs/toggle
         "<C-f8>" #'+treemacs/find-file)
 
-      "<f7>"     #'tototxt
+       "<f7>"     #'tototxt
 
       ;;; ivy
-      (:when (featurep! :completion ivy)
+       (:when (featurep! :completion ivy)
         :map ivy-minibuffer-map
         "TAB"    #'ivy-partial
         [tab]    #'ivy-partial)
 
       ;;; vc
-      (:when (featurep! :emacs vc)
+       (:when (featurep! :emacs vc)
         "C-x g"  #'magit-status)
-      )
+       )
 
-(remove-hook 'doom-first-buffer-hook #'smartparens-global-mode)
+      (remove-hook 'doom-first-buffer-hook #'smartparens-global-mode)
 
-;(setq doom-theme 'doom-one)
-(setq doom-theme 'doom-dracula)
+                                        ;(setq doom-theme 'doom-one)
+      (setq doom-theme 'doom-dracula)
 
-(load! "lisp/gud-enhancement")
+      (load! "lisp/gud-enhancement")
 
-(when (featurep! +lsp)
-  (make-lsp-client :new-connection (lsp-tramp-connection "clangd")
-                   :major-modes '(c-mode c++-mode)
-                   :remote? t
-                   :server-id 'clangd-remote))
+      (when (featurep! +lsp)
+        (make-lsp-client :new-connection (lsp-tramp-connection "clangd")
+                         :major-modes '(c-mode c++-mode)
+                         :remote? t
+                         :server-id 'clangd-remote))
 
-;(after! lsp-mode
-;  (set-lsp-priority! 'clangd 1))  ; ccls has priority 0
+                                        ;(after! lsp-mode
+                                        ;  (set-lsp-priority! 'clangd 1))  ; ccls has priority 0
 
-(after! magit
-  (setq magit-diff-refine-hunk 'all))
+      (after! magit
+        (setq magit-diff-refine-hunk 'all))
 
-(add-hook! magit-mode
-  (transient-append-suffix 'magit-push "-u"
-    '(1 "=s" "Skip gitlab pipeline" "--push-option=ci.skip"))
-  (transient-append-suffix 'magit-push "=s"
-    '(1 "=c" "Create mr" "--push-option=merge_request.create"))  ;; create a merge request
-  (transient-append-suffix 'magit-push "=c"
-    '(1 "=V" "Set CI variable" "--push-option=ci.variable="))  ;; Will prompt, can only set one extra variable
-  (transient-append-suffix 'magit-push "=V"
-    '(1 "=O" "Set push option" "--push-option="))  ;; Will prompt, can only set one extra push option
-  )
+      (add-hook! magit-mode
+        (transient-append-suffix 'magit-push "-u"
+          '(1 "=s" "Skip gitlab pipeline" "--push-option=ci.skip"))
+        (transient-append-suffix 'magit-push "=s"
+          '(1 "=c" "Create mr" "--push-option=merge_request.create"))  ;; create a merge request
+        (transient-append-suffix 'magit-push "=c"
+          '(1 "=V" "Set CI variable" "--push-option=ci.variable="))  ;; Will prompt, can only set one extra variable
+        (transient-append-suffix 'magit-push "=V"
+          '(1 "=O" "Set push option" "--push-option="))  ;; Will prompt, can only set one extra push option
+        )
 
-(add-hook! 'LaTeX-mode-hook
-  (add-to-list 'TeX-view-program-list '("Evince" "evince --page-index=%(outpage) %o"))
-  (setq TeX-view-program-selection '((output-pdf "Evince")))
-  )
+      (add-hook! 'LaTeX-mode-hook
+        (add-to-list 'TeX-view-program-list '("Evince" "evince --page-index=%(outpage) %o"))
+        (setq TeX-view-program-selection '((output-pdf "Evince")))
+        )
